7595af723b8ccfb04292cbdce13dc2529182f258
BigupJeff <me@jeffersonreal.uk>
2024-06-06 20:14:38 +0100

    npm/webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,13 @@ const path = require( 'path' )
 // Import the @wordpress/scripts config.
 const wordpressConfig = require( '@wordpress/scripts/config/webpack.config' )
 
-// Import the utility to auto-generate the entry points in the src directory.
-const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils/config' )
-
 module.exports = {
 	// Spread the existing WordPress config.
 	...wordpressConfig,
 
 	entry: {
-		// Spread the auto-generated entrypoints.
-		...getWebpackEntryPoints(),
+		// @wordpress/scripts helper which generates entry points from any '**/block.json' in 'src'.
+		...wordpressConfig.entry(),
 		'css/bigup-services-classic-editor': path.resolve( process.cwd(), 'src', 'css/bigup-services-classic-editor.scss' ),
 		'js/bigup-services-gutenberg': path.resolve( process.cwd(), 'src', 'js/bigup-services-gutenberg.js' ),
 		'js/bigup-services-classic-editor': path.resolve( process.cwd(), 'src', 'js/bigup-services-classic-editor.js' ),
